e6df648567415f5b73aedbc123fdb1956fb727fa
Michael Ding <mic.ding0725@gmail.com>
2017-07-24 14:48:51 +0800

    update readme

--- setup.py
+++ setup.py
@@ -9,16 +9,23 @@ setup(
     author_email='yandy.ding@gmail.com',
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Operating System :: POSIX',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    keywords='rpc',
+    keywords=['rpc', 'grpc'],
     packages=find_packages(exclude=['tests']),
     test_suite="tests",
+    install_requires=[
+        'grpcio>=1.4.0,<1.5.0'
+    ],
 )
