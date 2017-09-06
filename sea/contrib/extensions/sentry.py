282965abc4ffda461a78ef24b6f0dd6a311b02a3
Michael Ding <yandy.ding@gmail.com>
2017-09-06 19:51:39 +0800

    update template

--- sea/contrib/extensions/sentry.py
+++ sea/contrib/extensions/sentry.py
@@ -9,15 +9,16 @@ from sea.extensions import AbstractExtension
 class Sentry(AbstractExtension):
 
     def init_app(self, app):
-        client = raven.Client(
-            app.config.get('SENTRY_DSN'))
-        handler = SentryHandler(client)
-        handler.setLevel(logging.ERROR)
-        setup_logging(handler)
-        try:
-            from raven.contrib.celery import (
-                register_signal, register_logger_signal)
-            register_logger_signal(client)
-            register_signal(client)
-        except ImportError:
-            pass
+        dsn = app.config.get('SENTRY_DSN')
+        if dsn:
+            client = raven.Client(dsn)
+            handler = SentryHandler(client)
+            handler.setLevel(logging.ERROR)
+            setup_logging(handler)
+            try:
+                from raven.contrib.celery import (
+                    register_signal, register_logger_signal)
+                register_logger_signal(client)
+                register_signal(client)
+            except ImportError:
+                pass
