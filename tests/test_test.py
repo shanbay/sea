8ac50400735ca2fc4a1900e9f1133a31f75daf99
liuxiaoyu <xiaoyu.liu@shanbay.com>
2017-09-29 08:40:17 +0000

    add ctx of stub

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,3 +1,4 @@
+from sea.exceptions import BadRequestException
 from sea.test.stub import Context, Stub
 from sea.servicer import ServicerMeta
 
@@ -5,12 +6,20 @@ from sea.servicer import ServicerMeta
 def test_stub(app):
     class HelloServicer(metaclass=ServicerMeta):
 
+        def return_error(self, request, context):
+            raise BadRequestException()
+
         def return_normal(self, request, context):
             return context.invocation_metadata()
 
     stub = Stub(HelloServicer())
-    data = {'a': 1}
+    data = {'a': 2}
     res = stub.return_normal(None, metadata=data)
     assert res == data
+    assert stub.ctx.invocation_metadata() == data
     res = stub.return_normal(None)
     assert res is None
+    assert stub.ctx.invocation_metadata() is None
+
+    res = stub.return_error(None)
+    assert stub.ctx.code == BadRequestException.code
