74c662e4b7f9ae41714aa2fd4598c41d68ea1596
Hanaasagi <ambiguous404@gmail.com>
2017-10-18 11:21:07 +0800

    refactor cache model

--- sea/contrib/extensions/orator/cache_model.py
+++ sea/contrib/extensions/orator/cache_model.py
@@ -3,7 +3,6 @@ from orator.orm import model
 from sea import current_app
 from sea.utils import import_string
 from sea.contrib.extensions.cache import default_key, CacheNone
-from orator.exceptions.orm import ModelNotFound
 
 
 def _model_cache_key(f, cls, *args, **kwargs):
@@ -71,9 +70,13 @@ class ModelMeta(model.MetaModel):
         @cache.cached(
             cache_key=_model_cache_key,
             fallbacked=_find_register,
-            unless=_id_is_list, cache_none=True)
+            cache_none=True)
         def find(cls, id, columns=None):
-            if isinstance(id, list) and id and len(id) <= max_find_many_cache:
+            return super(cls, cls).find(id, columns)
+
+        @classmethod
+        def find_many(cls, id, columns=None):
+            if id and len(id) <= max_find_many_cache:
                 keymap = {i: _model_cache_key(None, cls, i) for i in id}
                 rv = cache.get_many(keymap.values())
                 models = dict(zip(id, rv))
@@ -85,47 +88,26 @@ class ModelMeta(model.MetaModel):
                     if not (m is CacheNone or m is None)}
                 if not missids:
                     return cls().new_collection(models.values())
-                missed = super(cls, cls).find(missids, columns)
+                missed = super(cls, cls).find_many(missids, columns)
                 missed = {m.id: m for m in missed}
                 models.update(missed)
                 key_model_map = {keymap[i]: m for i, m in missed.items()}
                 cache.set_many(key_model_map)
                 _bulk_register_to_related_caches(cls, key_model_map)
                 return cls().new_collection(list(models.values()))
-            return super(cls, cls).find(id, columns)
-
-        @classmethod
-        def find_or_fail(cls, id, columns=None):
-            result = cls.find(id, columns)
-            if isinstance(id, list):
-                if len(result) == len(set(id)):
-                    return result
-            elif result and result is not CacheNone:
-                return result
-
-            raise ModelNotFound(cls)
+            return super(cls, cls).find_many(id, columns)
 
         @classmethod
         @cache.cached(
             cache_key=_model_cache_key,
             fallbacked=_find_by_register, cache_none=True)
         def find_by(cls, name, val, columns=None):
-            return cls.where(name, '=', val).first(columns)
-
-        @classmethod
-        def find_by_or_fail(cls, name, val, columns=None):
-            result = cls.find_by(name, val, columns)
-
-            if result and result is not CacheNone:
-                return result
-
-            raise ModelNotFound(cls)
+            return super(cls, cls).find_by(name, val, columns)
 
         kws.update({
             'find': find,
             'find_by': find_by,
-            'find_or_fail': find_or_fail,
-            'find_by_or_fail': find_by_or_fail
+            'find_many': find_many,
         })
         return super().__new__(mcls, name, bases, kws)
 
