25b2952a8c25286354707bd2bd7a7c4fd03b12a0
Michael Ding <yandy.ding@gmail.com>
2018-03-09 19:00:51 +0800

    fix setup error

--- sea/__init__.py
+++ sea/__init__.py
@@ -4,7 +4,7 @@ import sys
 from sea.utils import import_string
 from sea.local import Proxy
 
-__version__ = '0.12.0'
+__version__ = '0.12.1'
 _app = None
 
 
--- setup.py
+++ setup.py
@@ -67,7 +67,6 @@ setup(
         ],
         'sea.jobs': [
             'celery=sea.contrib.extensions.celery.cmd:main',
-            'peewee=sea.contrib.extensions.peewee.cmd:main'
         ]
     }
 )
