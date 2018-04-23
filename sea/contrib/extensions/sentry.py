fb70cc1d82a9f26e51a01e3da75746269fc85715
StdioA <stdioa@163.com>
2018-04-23 15:48:45 +0800

    Supporting setup sentry client in app config

--- sea/contrib/extensions/sentry.py
+++ sea/contrib/extensions/sentry.py
@@ -9,14 +9,15 @@ class Sentry:
     def init_app(self, app):
         dsn = app.config.get('SENTRY_DSN')
         if dsn:
-            client = raven.Client(dsn)
-            handler = SentryHandler(client)
+            config = app.config.get_namespace('SENTRY_')
+            self.client = raven.Client(**config)
+            handler = SentryHandler(self.client)
             handler.setLevel(logging.ERROR)
             setup_logging(handler)
             try:
                 from raven.contrib.celery import (
                     register_signal, register_logger_signal)
-                register_logger_signal(client)
-                register_signal(client)
+                register_logger_signal(self.client)
+                register_signal(self.client)
             except ImportError:
                 pass
