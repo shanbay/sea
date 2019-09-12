0cdf144b32e1c327ca5a6a43d307854f164d6677
youcai li <youcai.li@shanbay.com>
2019-09-12 17:21:45 +0800

    拆分 celery 为 async_task 和 bus

--- sea/contrib/extensions/celery/cmd.py
+++ sea/contrib/extensions/celery/cmd.py
@@ -5,8 +5,14 @@ from celery.__main__ import main as celerymain
 from sea.cli import jobm
 
 
-@jobm.job('celery', proxy=True, help='invoke celery cmds')
-def main(argv):
+@jobm.job('async_task', proxy=True, help='invoke celery cmds for async tasks')
+def async_task(argv):
     sys.argv = ['celery'] + argv + \
-        ['-A', 'app.extensions:celeryapp']
+        ['-A', 'app.extensions:async_task']
+    return celerymain()
+
+@jobm.job('bus', proxy=True, help='invoke celery cmds for bus')
+def bus(argv):
+    sys.argv = ['celery'] + argv + \
+        ['-A', 'app.extensions:bus']
     return celerymain()
--- tests/wd/configs/default/__init__.py
+++ tests/wd/configs/default/__init__.py
@@ -1,17 +1,17 @@
 from .peewee import *
-from .celery import *
+from .async_task import *
 
-CACHE_BACKEND = 'Redis'
+CACHE_BACKEND = "Redis"
 
 MIDDLEWARES = [
-    'sea.middleware.ServiceLogMiddleware',
-    'sea.middleware.RpcErrorMiddleware'
+    "sea.middleware.ServiceLogMiddleware",
+    "sea.middleware.RpcErrorMiddleware",
 ]
 
-REDIS_HOST = 'localhost'
+REDIS_HOST = "localhost"
 REDIS_PORT = 6379
 REDIS_DB = 0
 
-TIMEZONE = 'Asia/Shanghai'
-GRPC_GRACE =0
+TIMEZONE = "Asia/Shanghai"
+GRPC_GRACE = 0
 PROMETHEUS_SCRAPE = True
