cf91020609259282dadd74e8cf4ad734bdea8d7c
sljeff <shanliangjeff@qq.com>
2019-11-20 16:59:26 +0800

    inspect时不create_app

--- sea/contrib/extensions/celery/cmd.py
+++ sea/contrib/extensions/celery/cmd.py
@@ -1,22 +1,34 @@
+import os
 import sys
 
 from celery.__main__ import main as celerymain
 
 from sea.cli import jobm
+from sea import create_app
 
 
 def celery(argv, app):
+    if argv[0] == "inspect":
+        import importlib
+        config_name = os.environ.get("SEA_ENV")
+        config = importlib.import_module('configs.{}'.format(config_name))
+        extensions = importlib.import_module("app.extensions")
+        celeryapp = getattr(extensions, app)
+        celeryapp.config_from_object(config, namespace=f"{app}_".upper())
+    else:
+        create_app()
     sys.argv = (
         ["celery"] + argv + ["-A", "app.extensions:{app}".format(app=app)]
     )
     return celerymain()
 
 
-@jobm.job("async_task", proxy=True, help="invoke celery cmds for async tasks")
+@jobm.job("async_task", proxy=True, inapp=False,
+          help="invoke celery cmds for async tasks")
 def async_task(argv):
     return celery(argv, "async_task")
 
 
-@jobm.job("bus", proxy=True, help="invoke celery cmds for bus")
+@jobm.job("bus", proxy=True, inapp=False, help="invoke celery cmds for bus")
 def bus(argv):
     return celery(argv, "bus")
--- tests/test_contrib/test_extensions/test_celery.py
+++ tests/test_contrib/test_extensions/test_celery.py
@@ -1,4 +1,7 @@
+import os
+import sys
 import mock
+import importlib
 from sea.contrib.extensions.celery import AsyncTask, Bus
 from sea.contrib.extensions.celery.cmd import async_task, bus
 
@@ -10,3 +13,28 @@ def test_celery(app):
         async_task(["worker"])
         bus(["worker"])
     # assert c.conf.broker_url == 'redis://localhost:6379/2'
+
+
+def test_celery_no_app():
+    """需要独立于其他测试的session单独跑一次"""
+    root_path = os.path.join(os.path.dirname(__file__).replace(
+        '/test_contrib/test_extensions', ''), 'wd')
+
+    if root_path not in sys.path:
+        """单独跑"""
+        os.environ.setdefault('SEA_ENV', 'testing')
+        sys.path.append(root_path)
+        extensions = importlib.import_module("app.extensions")
+        celeryapp = extensions.async_task
+        assert celeryapp.conf["broker_url"] is None
+    else:
+        """一起跑（app.extensions已经被初始化）"""
+        extensions = importlib.import_module("app.extensions")
+        celeryapp = extensions.async_task
+        assert celeryapp.conf["broker_url"] == 'redis://localhost:6379/2'
+
+    with mock.patch("sea.contrib.extensions.celery.cmd.celerymain") as mocked:
+        async_task("inspect ping -d wd@$HOSTNAME".split())
+        assert mocked.called
+    assert celeryapp.conf["broker_url"] == "redis://localhost:6379/2"
+    assert celeryapp.conf["TASK_DEFAULT_QUEUE"] == "wd.celery"
