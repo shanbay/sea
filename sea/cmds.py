61a026b2bbc0e2b58c5214d9d53f78745767bb54
Kind Jeff <shanliangjeff@qq.com>
2020-03-12 18:54:54 +0800

    fix ipython color (#153)

--- sea/cmds.py
+++ sea/cmds.py
@@ -22,7 +22,7 @@ def console():
     ctx = {'app': current_app}
     try:
         from IPython import embed
-        h, kwargs = embed, dict(banner1=banner, user_ns=ctx)
+        h, kwargs = embed, dict(banner1=banner, user_ns=ctx, colors="neutral")
     except ImportError:
         import code
         h, kwargs = code.interact, dict(banner=banner, local=ctx)
